ee8ae8027b5ebdbef67b6192e8f01cfde2febf5d
UchithmaSenevirathne <uchithma@gmail.com>
2024-04-06 09:34:08 +0530

    updated

---
+++ js/js-arrays.js
@@ -0,0 +1,22 @@
+let fruits = []
+console.log('type of fruits : ', typeof fruits)
+console.log('fruits array : ', fruits)
+console.log('fruits array length : ', fruits.length)
+
+fruits = ['apple', 'orange', 'pineapple']
+console.log('fruits array : ', fruits)
+console.log('fruits array length : ', fruits.length)
+
+console.log('fruit : ', fruits[1])
+console.log('fruit : ', fruits[2])
+console.log('fruit : ', fruits[0])
+console.log('fruit : ', fruits[3])
+
+fruits[1] = 'grapes'
+console.log('fruits array : ', fruits)
+
+fruits[5] = 'woodapple'
+console.log('fruits array : ', fruits)
+
+console.log('fruit : ', fruits[3])
+console.log('fruit : ', fruits[4])
---
+++ js/js-dynamic-datatypes.js
@@ -0,0 +1,11 @@
+let testingVariable;
+console.log('type of testingVariable : ', typeof testingVariable)
+
+testingVariable = "Kamal"
+console.log('type of testingVariable : ', typeof testingVariable)
+
+if (typeof testingVariable === String){
+
+} else if (typeof testingVariable === undefined){
+    
+}
